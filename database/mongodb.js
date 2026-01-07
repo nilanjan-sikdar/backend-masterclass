49aef64ebcaabbd9b397dbdd515d585746ae5549
nilanjan-sikdar <nilanjansikdar66@gmail.com>
2026-01-07 21:33:27 +0530

    database-MongoDB-complete

---
+++ database/mongodb.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import {DB_URI, NODE_ENV} from "../config/env.js";
+
+if(!DB_URI){
+    throw new Error("Please provide DB_URI in the environment variables");
+}
+
+// setup mongoose connection
+const connectToDatabase = async () => {
+    try{
+        await mongoose.connect(DB_URI);
+        console.log("Mongoose connected successfully", NODE_ENV);
+    }catch(error){
+        console.error("Mongoose connection error", error);
+        process.exit(1);
+    }
+}
+
+export default connectToDatabase;
