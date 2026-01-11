f4f334064ff2896d9cdfe4afd315028a1d71ce17
nilanjan-sikdar <nilanjansikdar66@gmail.com>
2026-01-11 17:09:42 +0530

    completed-error-handling

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import {PORT} from './config/env.js';
-
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -14,6 +20,9 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+// error middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
     console.log(`server is running on http://localhost:${PORT}`);
